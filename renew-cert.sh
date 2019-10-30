5372f7c13c989f3c397ea66138e456660d7ba2cc
akira12 <gamer_akira@126.com>
2019-10-30 23:16:19 +0800

    add force renew option to avoid failure

--- renew-cert.sh
+++ renew-cert.sh
@@ -9,7 +9,7 @@ fi
 do_name=$1
 
 systemctl stop nginx
-~/.acme.sh/acme.sh --renew -d $do_name --ecc
+~/.acme.sh/acme.sh --renew -d $do_name --ecc --force
 ~/.acme.sh/acme.sh --installcert -d $do_name --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 systemctl start nginx
 echo
